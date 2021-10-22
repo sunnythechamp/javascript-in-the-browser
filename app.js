fe1e58c74579cf2258749d67b17603dbae033f75
sunnythechamp <meesunnyarora@gmail.com>
2021-10-22 22:24:25 +0530

    push

---
+++ app.js
@@ -0,0 +1,6 @@
+var textarea = document.querySelector("btn-textarea");
+const button = document.querySelector("btn-button");
+button.addEventListener('click', function () {
+    console.log("clicked")
+    
+});
