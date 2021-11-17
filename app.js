4e7f97603945e61247feeee338bd218ddb191a6e
sunnythechamp <meesunnyarora@gmail.com>
2021-11-17 10:14:14 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-var textarea = document.querySelector("btn-textarea");
-const button = document.querySelector("btn-button");
-button.addEventListener('click', function () {
-    console.log("clicked")
+var btnevent = document.querySelector("#btn-event");
+var inputText = document.querySelector("#input-text");
+var outputText = document.querySelector("#output-text");
+
+ var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function urlBack(text) {
+    return serverUrl + "?" + "text=" + text;
+}
+function errorhandler(error){
+    console.log('If there is error',error);
+}
+function add() {
+    var inpuText = inputText.value;
+    fetch(urlBack(inpuText))
+        .then(response => response.json())
+        .then(json => {
+            var TransText = json.contents.translated;
+             outputText.innerText = TransText.value;
+        })
+        .catch(errorhandler)
     
-});
+        
+};
+
+
+btnevent.addEventListener('click', add)
+
+
+
+
+
+
+
